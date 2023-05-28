3881a4d431233b3f179a815bd3eb22b3952bc9b6
Isak Jung <isack19999@gmail.com>
2023-05-28 21:57:24 +0900

    ThreadLocalLog

--- src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -1,4 +1,4 @@
-package hello.advanced.trace.hellotrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
@@ -6,39 +6,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component //싱글톤으로 사용하기 위해 스프링 빈으로 등록한다. 컴포넌트 스캔의 대상이 된다.
-public class HelloTraceV2 {
+//@Component
+public class FieldLogTrace implements LogTrace {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
-
+    
+    private TraceId traceIdHolder; //traceId 동기화, 동시성 이슈 발생
+    
+    @Override
     public TraceStatus begin(String message) {
-        TraceId traceId = new TraceId();
+        syncTraceId();
+        TraceId traceId = traceIdHolder;
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
                 traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
-    //V2에서 추가
-    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
-//        TraceId traceId = new TraceId();
-        TraceId nextId = beforeTraceId.createNextId();
-        Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX,
-                nextId.getLevel()), message);
-        return new TraceStatus(nextId, startTimeMs, message);
+    private void syncTraceId() {
+        if (traceIdHolder == null) {
+            traceIdHolder = new TraceId();
+        }else {
+            traceIdHolder = traceIdHolder.createNextId();
+        }
     }
 
-
-    public void end(TraceStatus status)
-    {
+    @Override
+    public void end(TraceStatus status) {
         complete(status, null);
     }
+
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
+
     private void complete(TraceStatus status, Exception e) {
         Long stopTimeMs = System.currentTimeMillis();
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
@@ -52,7 +56,18 @@ public class HelloTraceV2 {
                     addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
                     e.toString());
         }
+
+        releaseTraceId();
     }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null; //destroy
+        }else {
+            traceIdHolder = traceIdHolder.createPreviousId();
+        }
+    }
+
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
@@ -60,5 +75,4 @@ public class HelloTraceV2 {
         }
         return sb.toString();
     }
-
 }
--- src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -1,44 +1,48 @@
-package hello.advanced.trace.hellotrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component //싱글톤으로 사용하기 위해 스프링 빈으로 등록한다. 컴포넌트 스캔의 대상이 된다.
-public class HelloTraceV2 {
+public class ThreadLocalLogTrace implements LogTrace {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
 
+    //    private TraceId traceIdHolder; //traceId 동기화, 동시성 이슈 발생
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
+
+    @Override
     public TraceStatus begin(String message) {
-        TraceId traceId = new TraceId();
+        syncTraceId();
+        TraceId traceId = traceIdHolder.get();
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
                 traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
-    //V2에서 추가
-    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
-//        TraceId traceId = new TraceId();
-        TraceId nextId = beforeTraceId.createNextId();
-        Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX,
-                nextId.getLevel()), message);
-        return new TraceStatus(nextId, startTimeMs, message);
+    private void syncTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        }else {
+            traceIdHolder.set(traceId.createNextId());
+        }
     }
 
-
-    public void end(TraceStatus status)
-    {
+    @Override
+    public void end(TraceStatus status) {
         complete(status, null);
     }
+
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
+
     private void complete(TraceStatus status, Exception e) {
         Long stopTimeMs = System.currentTimeMillis();
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
@@ -52,7 +56,19 @@ public class HelloTraceV2 {
                     addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
                     e.toString());
         }
+
+        releaseTraceId();
     }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove(); //destroy
+        }else {
+            traceIdHolder.set(traceId.createPreviousId());
+        }
+    }
+
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
@@ -60,5 +76,4 @@ public class HelloTraceV2 {
         }
         return sb.toString();
     }
-
 }
